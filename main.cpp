bdf43df3b75ebc58ca4f4c3bd09ea823c678bb03
Your Name <you@example.com>
2017-05-30 13:38:22 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+	
+using namespace std;
+
+#include "header/mainheader.h"
+
+main()
+{
+   programming object;
+
+   object.input_value();
+   object.output_value();
+
+   return 0;
+}
